66157825df3e917b40f4262ff2f1008dc55fbbe6
Jonah Unuafe <fejisfejiro@gmail.com>
2024-02-21 14:49:49 +0100

    react plus typescript

---
+++ basics.ts
@@ -0,0 +1,47 @@
+//primitives: number, strings, booleans, null, undefined
+//More complex: arrays, objects
+//Function types, parameters
+
+
+//Primitive
+
+let age: number;
+
+age = 12;
+
+let userName: string;
+
+userName = 'Jonah';
+
+let isInstructor: boolean;
+
+isInstructor = true;
+
+//More complex types
+
+
+
+
+let hobbies: string[];
+
+hobbies = ['Sports', 'Cooking'];
+
+
+let person: {
+    name: string,
+    age: number,
+};
+
+person = {
+    name: 'Jonah',
+    age: 39
+}
+
+// person = {
+//     isEmployee: true;
+// }
+
+let people: {
+    name: string,
+    age: number,
+}[];
