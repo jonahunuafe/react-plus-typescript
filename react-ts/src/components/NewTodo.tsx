04360091d4d07fe62f303ec25aea96b312f6e0ca
Jonah Unuafe <fejisfejiro@gmail.com>
2024-02-22 14:08:14 +0100

    adding styling

--- react-ts/src/components/NewTodo.tsx
+++ react-ts/src/components/NewTodo.tsx
@@ -1,9 +1,10 @@
 import { useRef } from "react";
 
+import classes from './NewTodo.module.css';
 
-const NewTodo: React.FC<{onAddTodo:  (text: string) => void }> = (props) => {
+
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
     const todoTextInputRef = useRef<HTMLInputElement>(null);
-
 
     const submitHandler = (event: React.FormEvent) => {
         event.preventDefault();
@@ -16,9 +17,9 @@ const NewTodo: React.FC<{onAddTodo:  (text: string) => void }> = (props) => {
         }
 
         props.onAddTodo(enteredText);
-    }
+    };
 
-    <form onSubmit={submitHandler}>
+    <form onSubmit={submitHandler} className={classes.form}>
         <label htmlFor="text">Todo text</label>
         <input type="text" id="text" ref={todoTextInputRef} />
         <button>Add Todo</button>
