a2157dc41aae58319cf6088f1ff563ae850735a5
Jonah Unuafe <fejisfejiro@gmail.com>
2024-02-22 17:00:59 +0100

    updated app.tsx

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -4,7 +4,6 @@ import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
-
 function App() {
   const [todos, setTodos] = useState<Todo[]>([]);
 
