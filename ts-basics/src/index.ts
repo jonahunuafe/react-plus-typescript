07b714fc641e3d442c89c9303c110517821596e5
Jonah Unuafe <fejisfejiro@gmail.com>
2024-08-22 15:41:55 +0100

    private and public modidier

--- ts-basics/src/index.ts
+++ ts-basics/src/index.ts
@@ -166,11 +166,32 @@ const jonah: Admin = {
 // }
 
 class Person {
-    readonly city: string = "Jupiter"
+    private _courseCount = 1
+
+    readonly city: string = "Jaipur"
     constructor(
         public email: string, public name: string
     ) {
 
+    }
+    
+    private deleteToken() {
+        console.log("Token deleted");
+    }
+
+    get getAppleEmail(): string {
+        return `apple${this.email}`
+    }
+
+    get courseCount(): number {
+        return this._courseCount
+    }
+
+    set courseCount(courseNum) {
+        if(courseNum <= 1) {
+            throw new Error("Course count should not be more than 1")
+        }
+        this._courseCount = courseNum
     }
 }
 
