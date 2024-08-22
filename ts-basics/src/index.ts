0e4fb76660bfe08cdf390ce5625d8c49bc1bb498
Jonah Unuafe <fejisfejiro@gmail.com>
2024-08-22 15:40:06 +0100

    getter and setter

--- ts-basics/src/index.ts
+++ ts-basics/src/index.ts
@@ -155,12 +155,22 @@ const jonah: Admin = {
 }
 
 // creating class in js using ts
+// class Person {
+//     public email: string
+//     private name: string
+//     readonly city: string = "Jupiter"
+//     constructor(email: string, name: string) {
+//         this.email = email;
+//         this.name = name
+//     }
+// }
+
 class Person {
-    email: string
-    name: string
-    constructor(email: string, name: string) {
-        this.email = email;
-        this.name = name
+    readonly city: string = "Jupiter"
+    constructor(
+        public email: string, public name: string
+    ) {
+
     }
 }
 
