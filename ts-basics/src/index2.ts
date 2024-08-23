09f7a415cec46c93748826d9702ef3f3a8ba4aee
Jonah Unuafe <fejisfejiro@gmail.com>
2024-08-23 06:46:43 +0100

    instanceof narrowing

--- ts-basics/src/index2.ts
+++ ts-basics/src/index2.ts
@@ -140,3 +140,24 @@ function logValue(x: Date | string) {
 }
 
 
+type Fish = {
+    swim: () => void
+}
+
+type Bird = {
+    fly: () => void
+}
+
+function isFish(pet: Fish | Bird): pet is Fish {
+    return (pet as Fish).swim !== undefined
+}
+
+function getFood(pet: Fish | Bird) {
+    if(isFish(pet)) {
+        pet
+        return "fish food"
+    } else {
+        pet
+        return "bird food"
+    }
+}
