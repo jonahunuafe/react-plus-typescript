4ce7d4a1e15f2df3e0513527ee35c4be1004f8fa
Jonah Unuafe <fejisfejiro@gmail.com>
2024-08-30 18:33:24 +0100

    handling change event with typescript

--- react-ts/src/components/Person.tsx
+++ react-ts/src/components/Person.tsx
@@ -9,22 +9,22 @@ export interface Props {
 
 export const Person = (props: Props) => {
     const [personBio, setPersonBio] = useState<string | null>("")
-    
+
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setPersonBio(event.target.value)
+    }
 
   return (
     <div>
-        {showInfo && (
-            <>
-                <p>{props.name}</p>
-                <p>{props.age}</p>
-                <p>This person {props.isMarried ? "is married" : "is single"}</p>
-            </>
-        )}
+        <p>{props.name}</p>
+        <p>{props.age}</p>
+        <p>This person {props.isMarried ? "is married" : "is single"}</p>
+        
         <p>
           {props.name} Bio: {!personBio ? "No Bio available" : personBio}
         </p>
 
-        <input />
+        <input onChange={handleChange} />
     </div>
   )
 }
